b0eba7f7eab1a33c1982e52c8e2149ed5e3866ea
Leandro Rios <leandroprios@gmail.com>
2022-12-20 09:06:07 -0300

    prueba de edicion 2

--- src/monitors/monitorROAS.js
+++ src/monitors/monitorROAS.js
@@ -1,5 +1,5 @@
 import Monitor from "./monitor";
-import from "md5";
+import md5 from "md5";
 import { getRelevant, diff } from "../utils/rpkiDiffingTool";
 import {AS} from "../model";
 import moment from "moment";
